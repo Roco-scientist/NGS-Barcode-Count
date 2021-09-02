3e1e08f510112a3e1c88b9c0b3cceb19c617232a
Rory Coffey <coffeyrt@gmail.com>
2021-09-02 19:42:51 -0400

    moved activity to separate mods and made more multithreaded

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,37 @@
 use clap::{App, Arg};
+use num_cpus;
 use rayon;
-use rayon::prelude::*;
-use std::fs::File;
-use std::io::{self, BufRead};
-// use std::path::Path;
+// use rayon::prelude::*;
 use std::sync::{Arc, Mutex};
 
 fn main() {
-    let (fastq, _format) = arguments().unwrap_or_else(|err| panic!("Argument error: {}", err));
+    let (fastq, _format, threads) =
+        arguments().unwrap_or_else(|err| panic!("Argument error: {}", err));
     rayon::scope(|s| {
-        let seq = Arc::new(Mutex::new(None));
+        let seq = Arc::new(Mutex::new(Vec::new()));
         let seq_clone = Arc::clone(&seq);
 
         let finished = Arc::new(Mutex::new(false));
         let finished_clone = Arc::clone(&finished);
         s.spawn(move |_| {
-            let fastq_file =
-                File::open(fastq).unwrap_or_else(|err| panic!("Error reading fastq file: {}", err));
-            let mut line_num = 1;
-            for line in io::BufReader::new(fastq_file).lines() {
-                if line_num == 2 {
-                    if let Ok(sequence_data) = line {
-                        while seq_clone.lock().unwrap().is_some() {}
-                        *seq_clone.lock().unwrap() = Some(sequence_data);
-                    }
-                }
-                line_num += 1;
-                if line_num == 5 {
-                    line_num = 1
-                }
-            }
+            del::read_fastq(fastq, seq_clone).unwrap();
             println!("finished");
             *finished_clone.lock().unwrap() = true;
         });
 
-        let seq_clone = Arc::clone(&seq);
-        let finished_clone = Arc::clone(&finished);
-        s.spawn(move |_| loop {
-            while seq_clone.lock().unwrap().is_none() {
-                if *finished_clone.lock().unwrap() {
-                    break;
-                }
-            }
-            if *finished_clone.lock().unwrap() {
-                break;
-            }
-            println!("Sequence: {}", seq_clone.lock().unwrap().as_ref().unwrap());
-            *seq_clone.lock().unwrap() = None;
-        })
+        for _ in 1..threads {
+            let seq_clone = Arc::clone(&seq);
+            let finished_clone = Arc::clone(&finished);
+            s.spawn(move |_| {
+                del::parse_sequences::parse(seq_clone, finished_clone).unwrap();
+            })
+        }
     });
 }
 
 /// Gets the command line arguments
-pub fn arguments() -> Result<(String, String), Box<dyn std::error::Error>> {
+pub fn arguments() -> Result<(String, String, u8), Box<dyn std::error::Error>> {
+    let total_cpus = num_cpus::get().to_string();
     // parse arguments
     let args = App::new("DEL analysis")
         .version("0.1")
@@ -73,6 +52,14 @@ pub fn arguments() -> Result<(String, String), Box<dyn std::error::Error>> {
                 .takes_value(true)
                 .help("Sequence format file"),
         )
+        .arg(
+            Arg::with_name("threads")
+                .short("t")
+                .long("threads")
+                .takes_value(true)
+                .default_value(&total_cpus)
+                .help("Number of threads"),
+        )
         .get_matches();
 
     return Ok((
@@ -80,5 +67,6 @@ pub fn arguments() -> Result<(String, String), Box<dyn std::error::Error>> {
         args.value_of("sequence_format")
             .unwrap_or("nothing")
             .to_string(),
+        args.value_of("threads").unwrap().parse::<u8>().unwrap(),
     ));
 }
