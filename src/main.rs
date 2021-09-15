c252af171933be681df5313498e25451be7ca1d9
Rory Coffey <coffeyrt@gmail.com>
2021-09-15 19:27:19 -0400

    changed to version 0.4.0

--- src/main.rs
+++ src/main.rs
@@ -178,7 +178,7 @@ pub fn arguments() -> Result<
     let today = Local::today().format("%Y-%m-%d").to_string();
     // parse arguments
     let args = App::new("DEL analysis")
-        .version("0.3.2")
+        .version("0.4.0")
         .author("Rory Coffey <coffeyrt@gmail.com>")
         .about("Counts DEL hits from fastq files and optional does conversions of sample IDs and building block IDs")
         .arg(
